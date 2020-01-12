25036e6baf4b6cbef4a822a12a1081142519912a
furqandar <furqandarsf@gmail.com>
2020-01-12 19:16:59 +0500

    create select function for dropdown list

--- employee_project/employee_register/forms.py
+++ employee_project/employee_register/forms.py
@@ -2,7 +2,18 @@ from django import forms
 from .models import employee
 
 class employeeform(forms.ModelForm):
+
     class Meta:
         model = employee
-        fields = ('fullname','emp_code','mobile','position')
+        fields = ('fullname','mobile','emp_code','position')
+        labels = {
+            'fullname':'Full Name',
+            'emp_code':'EMP. Code'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(employeeform,self).__init__(*args, **kwargs)
+        self.fields['position'].empty_label = "Select"
+        self.fields['emp_code'].required = False
+
 
--- employee_project/employee_register/models.py
+++ employee_project/employee_register/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class position(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
 
 class employee(models.Model):
 
