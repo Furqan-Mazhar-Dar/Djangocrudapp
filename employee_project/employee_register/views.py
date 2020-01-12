6ab0eb5c189fae954e524bb0458b688cad9c52b6
furqandar <furqandarsf@gmail.com>
2020-01-12 19:34:51 +0500

    create some function in vies

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import employeeform
+from .models import employee
 
 # Create your views here.
 def employee_list(request):
     return render(request,"employee_register/employee_list.html")
 
 def employee_form(request):
-    form = employee_form()
-    return render(request,"employee_register/employee_form.html",form)
+    if request.method == "GET":
+        if id == 0:
+            form = employeeform()
+        else:
+            employee = employee.objects.get(pk=id)
+            form = employeeform(instance=employee)
+        return render(request, "employee_register/employee_form.html", {'form': form})
+    else:
+        if id == 0:
+            form = employeeform(request.POST)
+        else:
+            employee = employee.objects.get(pk=id)
+            form = employeeform(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+        return redirect('/employee/list')
 
 def employee_delete(request):
-    return
+    employee = employee.objects.get(pk=id)
+    employee.delete()
+    return redirect('/employee/list')
